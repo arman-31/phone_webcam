de12f045e4d4e44f908cfdbda98ecc55064725fc
Arman <armanboncales31@gmail.com>
2025-02-04 22:12:46 +0800

    commited

--- update.py
+++ update.py
@@ -4,10 +4,10 @@ def check_for_update():
     latest_version_url = "https://raw.githubusercontent.com/arman-31/phone_webcam/main/latest_version.txt"
     current_version ="1.0.0"
     
-    response = requests,get(latest_version_url)
+    response = requests.get(latest_version_url)
     latest_version_url = response.txt.strip()
     
-    if latest_versio > current_version:
+    if latest_version > current_version:
         print("Update available! Downloading..")
         
 check_for_update()
